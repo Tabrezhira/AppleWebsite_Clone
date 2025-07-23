76456835503e68bd6bec950343d69f91c6e200dc
Tabrezhira <Tabrez.hira@gmail.com>
2025-07-23 19:24:44 +0530

    nav and hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import {appleImg, bagImg, searchImg} from '../utils'
-
+import{navLists} from '../constants/index.js'
 
 function Navbar() {
   return (
@@ -7,13 +7,13 @@ function Navbar() {
       <nav className='flex w-full screen-max-width '>
         <img src={appleImg} alt="Apple" width={14}/>
         <div className='flex flex-1 justify-center max-sm:hidden'>
-          {['Phone','Macbooks','Tablets'].map((nav) => (
-            <div key={nav}>
+          {navLists.map((nav) => (
+            <div key={nav} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>
                 {nav}
             </div>
           ))}
         </div>
-        <div>
+        <div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
           <img src={searchImg} alt="search" width={18} height={18} />
           <img src={bagImg} alt="search" width={18} height={18} />
         </div>
